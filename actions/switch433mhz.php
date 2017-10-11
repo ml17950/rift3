51571f089715bb321c7dad7f42eb77d488456230
ml17950 <ml17950@gmail.com>
2017-10-11 16:57:19 +0200

    added basic actions

---
+++ actions/switch433mhz.php
@@ -0,0 +1,23 @@
+<?php
+	function switch433mhz($on_param, $off_param, $onoff) {
+		switch ($onoff) {
+			case ON:
+				//$cmd = 'send 00001 1 1';
+				$cmd = 'send '.$on_param;
+				$output = array();
+				for ($i=1; $i<=2; $i++) {
+					@exec($cmd, $output, $retval);
+				}
+				break;
+			
+			case OFF:
+				//$cmd = 'send 00001 1 0';
+				$cmd = 'send '.$off_param;
+				$output = array();
+				for ($i=1; $i<=2; $i++) {
+					@exec($cmd, $output, $retval);
+				}
+				break;
+		}
+	}
+?>
