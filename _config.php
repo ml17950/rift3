08f0ae47bcd1a7316ca227eb8cda05cc695b12ea
ml17950 <ml17950@gmail.com>
2017-10-12 08:29:58 +0200

    bugfixes

--- _config.php
+++ _config.php
@@ -1,5 +1,6 @@
 <?php
-	error_reporting(E_ALL);
+	//error_reporting(E_ALL);
+	error_reporting(E_ALL & ~E_NOTICE);
 	ini_set('display_errors', 1);
 	
 	define('ABSPATH', dirname(__FILE__));
@@ -16,4 +17,12 @@
 	define('OFF2ON', 'OFF2ON');
 	
 	define('UNKNOWN', 'UNKNOWN');
+	
+	// ###########################################
+	// ### change only defines below this line ###
+	// ###########################################
+	
+	date_default_timezone_set('Europe/Berlin');
+	
+
 ?>
